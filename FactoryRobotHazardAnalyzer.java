a8489f62c192cd7b8431331a368e7bffe87dc591
mehul1110 <mehulshra1110@gmail.com>
2026-01-29 16:31:19 +0530

    UC1: Display static system message

---
+++ FactoryRobotHazardAnalyzer.java
@@ -0,0 +1,11 @@
+/**
+ * FactoryRobotHazardAnalyzer
+ *
+ * Main entry point for the Factory Robot Hazard Analyzer application.
+ * Currently displays a static welcome message.
+ */
+public class FactoryRobotHazardAnalyzer {
+    public static void main(String[] args) {
+        System.out.println("Factory Robot Hazard Analyzer");
+    }
+}
